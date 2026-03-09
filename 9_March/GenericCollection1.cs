2d00ebf30542a3e3ef044986052a5ea8c0282129
Kaverirajput <kr561516@gmail.com>
2026-03-09 14:19:09 +0530

    Csharp generic collection program

---
+++ 9_March/GenericCollection1.cs
@@ -0,0 +1,24 @@
+﻿
+
+class GenericCollection1
+{
+
+    void adddata()
+    {
+        List<string> name = new List<string>();
+        name.Add("kaveri");
+        name.Add("gayatri");
+        name.Add("Goldy");
+
+        foreach (string s in name)
+        {
+            Console.WriteLine(s);
+        }
+
+    }
+    public static void Main(String[] args)
+    {
+        GenericCollection1 a = new GenericCollection1();
+        a.adddata();
+    }
+}
---
+++ 9_March/Generic_Collection.cs
@@ -0,0 +1,63 @@
+﻿//remove 345 using index
+//add 200 and 8 integer at index 0
+// check weather 8787 object is present in the collection or not
+//print the count of all objects in the collection 
+//reverse the collection
+
+class Generic_Collection
+{
+    static List<int> numbers;//declaring a list of integers
+    public static void integerobjets()
+    {
+        numbers = new List<int>();//initializing the list of integers
+        numbers.Add(744);
+        numbers.Add(345);
+        numbers.Add(7588);
+        numbers.Add(982738);
+
+
+    }
+    static void printdata()
+    {
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    void generic()
+    {
+        integerobjets();
+        printdata();
+
+        Console.WriteLine("---------------------------");
+
+        Console.WriteLine("Conatains : " + numbers.Contains(8787));
+
+        Console.WriteLine("-----------------------------");
+
+        Console.WriteLine("count of elements : " + numbers.Count);
+
+        Console.WriteLine("---------------------------");
+        Console.WriteLine("removing 345 ");
+        numbers.RemoveAt(1);
+        printdata();
+
+        Console.WriteLine("---------------------------");
+        Console.WriteLine("reverse the collection : ");
+        numbers.Reverse();
+        printdata();
+
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("inserting 200");
+        numbers.Insert(0, 200);
+        printdata();
+
+    }
+    public static void Main(String[] args)
+    {
+        Generic_Collection g = new Generic_Collection();
+
+        g.generic();
+
+    }
+}
